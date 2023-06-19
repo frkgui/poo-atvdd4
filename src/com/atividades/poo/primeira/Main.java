fe3179178dd206778da3a4548565ccc5dc66fc15
frkgui <78104494+frkgui@users.noreply.github.com>
2023-06-19 18:34:30 -0300

    add methods

--- src/com/atividades/poo/primeira/Main.java
+++ src/com/atividades/poo/primeira/Main.java
@@ -1,2 +1,31 @@
-package com.atividades.poo.primeira;public class Main {
+package com.atividades.poo.primeira;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    List<Integer> minhaLista = new ArrayList<>();
+
+
+
+    }
+
+    public static int somarListas(List<Integer> listaDeNumeros){
+
+        Stream<Integer> streamLista = listaDeNumeros.stream();
+
+        Stream<Integer> streamFilterPares = streamLista.filter(numero -> numero % 2 == 0);
+
+        int soma = streamFilterPares.reduce(0,(a,b)-> a+b);
+
+        return soma ;
+
+    }
+
+
+
 }
